c176125bb2eb9ced11566e83f76713c48815c8fd
Phelipe Pereira <phelipescript@gmail.com>
2023-01-22 20:43:59 -0300

    :rocket: class 05 - Next Level

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState, useCallback } from 'react'
 import { Text, View, ScrollView, Alert } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import dayjs from 'dayjs';
 
 import { api } from '../lib/axios'
@@ -41,9 +41,9 @@ export function Home() {
     }
   }
 
-  useEffect(() => {
+  useFocusEffect(useCallback(() => {
     fetchData()
-  }, [])
+  }, []))
 
   if (loading) {
     return (
